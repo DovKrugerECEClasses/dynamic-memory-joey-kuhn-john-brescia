9a8e58bfb8fad15bc9b7c73b60cecf72060e2f4b
Joey Kuhn <jkuhn@stevens.edu>
2018-11-28 23:40:21 -0500

    fff

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,4 +1,13 @@
+/*
+    HW - STL
+    Authors: Joseph Kuhn & John Brescia
+    We pledge our honor that we have abided by the Stevens Honor System.
+*/
+
 #include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
 
 /*
 	Cube
@@ -8,11 +17,61 @@ https://en.wikipedia.org/wiki/STL_(file_format)
 
  */
 
+class Cube {
+private:
+    double dimension, xval, yval, zval;
+
+public:
+    Cube(double d) : dimension(d){
+    }
+
+    void translate(double x, double y, double z) {
+        dimension += x;
+        dimension += y;
+        dimension += z;
+    }
+
+    double volume(){
+        return dimension * dimension * dimension;
+    }
+
+    void write(const string& name){
+
+    }
+};
+
+class Cylinder {
+private:
+    double radius, height;
+public:
+    Cylinder(double radius, double height) : radius(radius), height(height){
+    }
+
+    
+};
+
+class Design {
+private:
+
+public:
+
+    Design() {
+
+    }
+
+    void add(Cube c)
+    {
+
+    }
+};
+
+
+
 int main() {
     Design d;
     double s = 10;
     Cube c(s);
-    c.translate(x,y,z);
+    //c.translate(x,y,z);
     // rotate??
     cout << c.volume() << '\n';
     c.write("out.stl"); // how to get all my shapes into the same file
